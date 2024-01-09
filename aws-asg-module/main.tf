8a86f60b32e5348db24305f5c66e08e3e23f25b5
Shashwat Pandey Vatsa Plivo <145681621+vatsashashwatpandey@users.noreply.github.com>
2024-01-10 04:41:17 +0530

    Add files via upload

---
+++ aws-asg-module/main.tf
@@ -0,0 +1,41 @@
+provider "github" {
+  token = "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"
+}
+
+# Repository details
+resource "github_repository" "aws_asg_module" {
+  name        = "terraform-aws-asg-module"
+  description = "Terraform module for setting up AWS ASG"
+  private     = false
+
+  # GitHub topics for the repository
+  topics      = ["terraform", "aws", "asg", "module"]
+
+  # Template for repository settings
+  template = {
+    owner      = "plivo"
+    repository = "terraform-module-template"
+  }
+
+  # GitHub teams with admin access
+  admin_teams = [
+    "devops"
+  ]
+
+  # GitHub teams with pull (read-only) access
+  pull_teams = [
+    "coengage",
+    "functional_callinsights",
+    "functional_central",
+    "functional_clientsdk",
+    "functional_financialsystems",
+    "functional_messaging",
+    "functional_numbers",
+    "functional_phloandconsole",
+    "functional_sre",    
+    "functional_sellular",
+    "functional_serversdk",
+    "functional_voice",
+    "functional_voice_api",
+    "functional_zentrunk"
+}
